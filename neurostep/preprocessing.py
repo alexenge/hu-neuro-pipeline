deb14041c113e4ca3149606e2e69bce721046098
Alexander Enge <alexander.enge@hu-berlin.de>
2021-11-06 17:58:08 +0100

    Add docstring to preprocess

--- neurostep/preprocessing.py
+++ neurostep/preprocessing.py
@@ -23,14 +23,14 @@ def preprocess_single(
     heog_channels='auto',
     montage='easycap-M1',
     highpass_freq=0.1,
-    lowpass_freq=30,
+    lowpass_freq=30.0,
     epochs_tmin=-0.5,
     epochs_tmax=1.5,
-    baseline=(-0.2, 0),
+    baseline=(-0.2, 0.0),
     triggers=None,
     skip_log_rows=None,
-    reject_peak_to_peak=200,
-    reject_flat=1,
+    reject_peak_to_peak=200.0,
+    reject_flat=1.0,
     components_df=pd.DataFrame({
         'name': [], 'tmin': [], 'tmax': [], 'roi': []}),
     condition_cols=None,
@@ -151,14 +151,14 @@ def preprocess(
     heog_channels='auto',
     montage='easycap-M1',
     highpass_freq=0.1,
-    lowpass_freq=30,
+    lowpass_freq=30.0,
     epochs_tmin=-0.5,
     epochs_tmax=1.5,
-    baseline=(-0.2, 0),
+    baseline=(-0.2, 0.0),
     triggers=None,
     skip_log_rows=None,
-    reject_peak_to_peak=200,
-    reject_flat=1,
+    reject_peak_to_peak=200.0,
+    reject_flat=1.0,
     components_df=pd.DataFrame({
         'name': [], 'tmin': [], 'tmax': [], 'roi': []}),
     condition_cols=None,
@@ -170,6 +170,140 @@ def preprocess(
     to_df=True,
     n_procs='auto'
 ):
+    """Preprocesses EEG data for all participants of an experiment in parallel.
+
+    For each participant, reads raw data, performs downsampling (optional),
+    laods electrode locations, interpolates bad channels (optional), re-
+    references to common average, does ocular correction (ICA or MSEC/BESA,
+    optional), does filtering (optional), does segmenting into epochs, rejects
+    bad epochs, and computes single trial mean ERP amplitudes for components of
+    interest as well as condition averages (evokeds). At the group level,
+    combines all single trial behavioral data and mean amplitudes and combines
+    evokeds into grand averages (e.g., for plotting).
+
+    Parameters
+    ----------
+    vhdr_files : str | Path | list of str | list of Path
+        Raw EEG files for all participants. Can either be a list of `.vhdr`
+        file paths or the path of their parent directory.
+    log_files : str | Path | list of str | list of Path
+        Behavioral log files for all participants. Can either be a list of
+        `.txt`, `.log`, or `.csv` file paths or the path of their parent
+        directory.
+    ocular_correction : str | list of str | list of Path | None, default 'fastica'
+        Ocular correction method. Can either be an ICA method (`'fastica'` or
+        `'infomax'` or `'picard'`), a list of MSEC (BESA) `.matrix` file paths,
+        or their parent directory.
+    downsample_sfreq : float | None, default None
+        Downsampling frequency in Hz. Downsampling (e.g., from 500.0 Hz to
+        250.0 Hz) saves computation time and disk space.
+    bad_channels : list of list of str | 'auto' | None, default 'auto'
+        Bad channels for each participant that should be replaced via
+        interpolation. If 'auto', interpolate channels that exceed the
+        rejection threshold (see `reject_peak_to_peak` and `reject_flat`)
+        in more than 5% of all epochs.
+    veog_channels : list of str | 'auto' | None, default 'auto'
+        Names of two vertical EOG channels (above and below the eye) for
+        creating a virtual, bipolar VEOG channel. If 'auto', try different
+        common VEOG electrodes ('Fp1'/'FP1', 'Auge_u'/'IO1').
+    heog_channels : list of str | 'auto' | None, default 'auto'
+        Names of two vertical EOG channels (left and right of the eye) for
+        creating a virtual, bipolar HEOG channel. If 'auto', try different
+        common HEOG electrodes ('F9'/'Afp9', 'F10'/'Afp10').
+    montage : str | Path, default 'easycap-M1'
+        Montage for looking up channel locations. Can either be the name of a
+        standard montage (see [1]) or the path to a custom electrode location
+        file (see [2]).
+    highpass_freq : float | None, default 0.1
+        Pass-band edge (in Hz) for the highpass filter. If None, don't use
+        highpass filter.
+    lowpass_freq : float | None, default 30.0
+        Pass-band edge (in Hz) for the lowpass filter. If None, don't use
+        lowpass filter.
+    epochs_tmin : float, default -0.5
+        Starting point of the epoch (in s) relative to stimulus onset.
+    epochs_tmax : float, default 1.5
+        Ending point of the epoch (in s) relative to stimulus onset.    
+    baseline : tuple of (float, float), default (-0.2, 0.0)
+        Time window (in s) for baseline correction relative to stimulus onset.
+    triggers : dict of {str: int, ...} | None, default None
+        Triggers (values, int) of the relevant events (usually stimuli) and
+        their corresponding labels (keys, str) for creating epochs. If None,
+        use all triggers. This will not work if there are more triggers than
+        trials in the log file.
+    skip_log_rows : list of list of int | dict | None, default None
+        List of row indices from the log file file to skip for each
+        participant. Useful if the EEG was accidently paused or started late
+        for any participants. Can also be a dict {str: str or list of str, ...}
+        where keys are column names (in the log file) and values are one or
+        more conditions in these column. Useful if some conditions don't have
+        triggers (e.g., because they are filler trials).
+    reject_peak_to_peak : float | None, default 200.0
+        Peak-to-peak amplitude (in microvolts) for rejecting bad epochs.
+    reject_flat : float | None, default 1.0
+        Amplitude (in microvolts) for rejecting bad epochs as flat.
+    components_df : pandas.DataFrame | None, default None
+        Definition of ERP components for single trial analysis. Must have one
+        row per component and the following columns: 'name' (str), `tmin`
+        (float), `tmax` (float), `roi` (list of str), where `tmin` and `tmax`
+        are the time window of interest (in s) and `roi` is a list of channel
+        names for the spatial region of interest.
+    condition_cols : str | list of str | dict | None, default None
+        Columns in the log file to compute condition averages (evokeds) for.
+        Can be one more column names, in which one average is computed for each
+        condition (one column) or each combination of conditions (multiple
+        columns). Can also be a dict if multiple such sets of averages is
+        needed (e.g., separately for main effects and interactions). In this
+        case, keys (str) are labels for distinguishing output file names and
+        values are the same as before (one or more column names). If None,
+        create one average for each EEG trigger (see `triggers`).
+    clean_dir : str | Path | None, default None
+        Output directory to save the cleaned (ocular corrected, filtered)
+        continuous EEG data (always in `.fif` format) for each participant.
+        Not usually needed.
+    epochs_dir : str | Path | None, default None
+        Output directory to save the full (time-resolved) epochs data
+        (in `.fif` and/or `.csv` format, see `to_df`) for each participant.
+        Not usually needed.
+    trials_dir : str | Path | None, default None
+        Output directory to save the single trial behavioral and ERP component
+        DataFrame (always in `.csv` format) for each participant.
+    evokeds_dir : str | Path | None, default None
+        Output directory to save the condition averages (evokeds; in `.fif`
+        and/or `.csv` format, see `to_df`) for each participant; see
+        `condition_cols`.
+    export_dir : str | Path | None, default None
+        Output directory to save data at the group level, namely channel
+        locations (in `.csv` format), combined single trial and evoked data,
+        and grand averages (in `.fif` and/or `.csv` format, see `to_df`).
+    to_df : bool | 'both', default True
+        Convert all MNE objects (epochs, evokeds, grand averages) to data
+        frames and save them in `.csv` instead of `.fif` format. If `both`,
+        save in both `.csv` and `.fif` format.
+    n_procs : int | 'auto', default 'auto'
+        Number of CPU cores to use for processing participants in parallel. If
+        'auto', use all available cores on the machine minus one.
+
+    Returns
+    -------
+    trials : pandas.DataFrame
+        Combined single trial behavioral and ERP component data for all
+        participants. Can be used for running linear mixed models (LMMs) on
+        reaction times and ERP mean amplitudes.
+    evokeds_dict : dict of list of evokeds
+        One set of evokeds for each (combination of) condition(s) of interest
+        (see `condition_cols`). Each set contains the evokeds from all
+        participants as a list.
+    evokeds_dict : dict of pandas.DataFrame
+        Same as `evokeds_dict`, but converted to data frames. Each data frame
+        contains the evokeds from all participants. Can be used, e.g., for
+        plotting grand averages with uncertainty intervals.
+
+    Notes
+    -----
+    [1] https://mne.tools/stable/generated/mne.channels.make_standard_montage.html
+    [2] https://mne.tools/stable/generated/mne.channels.read_custom_montage.html
+    """
 
     # Create dict of non participant-specific inputs
     shared_kwargs = locals().copy()
