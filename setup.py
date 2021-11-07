b1d0dd753621e420e7be6015b9cceec302b875d1
Alexander Enge <alexander.enge@hu-berlin.de>
2021-11-07 15:47:09 +0100

    Rename to STEP-MNE

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='mne-step',
+    name='step-mne',
     version='0.0.1',
     author='Alexander Enge',
     author_email='alexander.enge@hu-berlin.de',
     description='Single trial EEG pipeline using MNE-Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/alexenge/mne-step',
+    url='https://github.com/alexenge/step-mne',
     project_urls={
-        'Issue trackers': 'https://github.com/alexenge/mne-step/issues',
+        'Issue trackers': 'https://github.com/alexenge/step-mne/issues',
     },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    packages=['mne_step'],
+    packages=['step_mne'],
     install_requires=[
         'chardet',
         'mne>=0.24.0',
