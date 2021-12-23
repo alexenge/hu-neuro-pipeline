13ec5b50c02b193a6515f27e1d5428abd5cb743e
Alexander Enge <alexander.enge@hu-berlin.de>
2021-12-23 15:30:01 +0100

    Add __version__

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,4 +1,14 @@
-"Single trial EEG pipeline at the Neurocognitive Psychology lab, HU Berlin"
+"""Single trial EEG pipeline at the Neurocognitive Psychology lab,
+Humboldt-Universität zu Berlin"""
 
+# Make central functions available as top level imports
 from .group import group_pipeline
 from .participant import participant_pipeline
+
+# Get current package version
+try:
+    from ._version import version as __version__
+    from ._version import version_tuple
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
