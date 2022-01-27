a42e40d419c4251c1817596b573f10b08c0f2879
Alexander Enge <alexander.enge@hu-berlin.de>
2022-01-27 19:10:07 +0100

    Return more cluster stats

--- pipeline/perm.py
+++ pipeline/perm.py
@@ -59,29 +59,47 @@ def compute_perm(evokeds_per_participant, contrasts, tmin=0., tmax=1.,
         channel_adjacency, _ = find_ch_adjacency(evoked.info, 'eeg')
 
         # Run permutation test
-        t_obs, clusters, cluster_pv, H0 = permutation_cluster_1samp_test(
+        t_obs, clusters, cluster_p_vals, H0 = permutation_cluster_1samp_test(
             X, n_permutations=n_permutations, adjacency=channel_adjacency,
             n_jobs=n_jobs, seed=seed)
 
-        # Create cluster image
-        cluster_arr = np.ones_like(t_obs)
+        # Create cluster images with cluster indices and p values
+        ixs = np.zeros_like(t_obs)
+        p_vals = np.ones_like(t_obs)
+        pos_ix = 0
+        neg_ix = 0
         for ix, cluster in enumerate(clusters):
-            pv = cluster_pv[ix]
-            cluster_arr[cluster] = pv
 
-        # Convert to DataFrame
-        cluster_df = pd.DataFrame(cluster_arr, index=times, columns=channels)
-        cluster_df = cluster_df.reset_index().rename(columns={'index': 'time'})
+            # Check if the cluster is positive or negative
+            if t_obs[cluster][0] > 0:
+                pos_ix += 1
+                ixs[cluster] = pos_ix
+            else:
+                neg_ix -= 1
+                ixs[cluster] = neg_ix
 
-        # Add info about the current contrast
-        contrast_str = ' - '.join(contrast)
-        cluster_df.insert(0, 'contrast', contrast_str)
+            # Extract cluster level p value
+            p_val = cluster_p_vals[ix]
+            p_vals[cluster] = p_val
+
+        # Convert to DataFrames, adding info about contrast, stat, time
+        dfs = []
+        arrs = [t_obs, ixs, p_vals]
+        stats = ['t_obs', 'cluster_index', 'p_val']
+        for arr, stat in zip(arrs, stats):
+            df = pd.DataFrame(arr, columns=channels)
+            df.insert(0, 'contrast', ' - '.join(contrast))
+            df.insert(1, 'stat', stat)
+            df.insert(2, 'time', times)
+            dfs.append(df)
+
+        # Combine DataFrames
+        cluster_df = pd.concat(dfs)
 
         # Append to the list of all contrasts
         cluster_dfs.append(cluster_df)
 
     # Combine DataFrames of all contrasts
-    # TODO: Maybe we should convert this to long format for easier plotting
     cluster_df = pd.concat(cluster_dfs, ignore_index=True)
 
     return cluster_df
@@ -154,33 +172,49 @@ def compute_perm_tfr(
         adjacency = combine_adjacency(n_freqs, channel_adjacency)
 
         # Run permutation test
-        t_obs, clusters, cluster_pv, H0 = permutation_cluster_1samp_test(
+        t_obs, clusters, cluster_p_vals, H0 = permutation_cluster_1samp_test(
             X, n_permutations=n_permutations, adjacency=adjacency,
             n_jobs=n_jobs, seed=seed)
 
-        # Create cluster image
-        cluster_arr = np.ones_like(t_obs)
+        # Create cluster images with cluster indices and p values
+        ixs = np.zeros_like(t_obs)
+        p_vals = np.ones_like(t_obs)
+        pos_ix = 0
+        neg_ix = 0
         for ix, cluster in enumerate(clusters):
-            pv = cluster_pv[ix]
-            cluster_arr[cluster] = pv
 
-        # Stack array so we can convert it to a DataFrame
-        cluster_arr = cluster_arr.reshape(n_times * n_freqs, n_channels)
+            # Check if the cluster is positive or negative
+            if t_obs[cluster][0] > 0:
+                pos_ix += 1
+                ixs[cluster] = pos_ix
+            else:
+                neg_ix -= 1
+                ixs[cluster] = neg_ix
 
-        # Convert to DataFrame
-        cluster_df = pd.DataFrame(cluster_arr, columns=channels)
+            # Extract cluster level p value
+            p_val = cluster_p_vals[ix]
+            p_vals[cluster] = p_val
 
-        # Add information about the current contrast, times, and frequencies
-        info_df = pd.DataFrame({'contrast': ' - '.join(contrast),
-                                'time': np.repeat(times, n_freqs),
-                                'freq': np.tile(freqs, n_times)})
-        cluster_df = pd.concat([info_df, cluster_df], axis=1)
+        # Convert to DataFrames, adding info about contrast, stat, time, freq
+        dfs = []
+        arrs = [t_obs, ixs, p_vals]
+        stats = ['t_obs', 'cluster_index', 'p_val']
+        for arr, stat in zip(arrs, stats):
+            arr = arr.reshape(n_times * n_freqs, n_channels)
+            df = pd.DataFrame(arr, columns=channels)
+            df.insert(0, 'contrast', ' - '.join(contrast))
+            df.insert(1, 'stat', stat)
+            df.insert(2, 'time', np.repeat(times, n_freqs))
+            df.insert(3, 'freq', np.tile(freqs, n_times))
+            dfs.append(df)
+
+        # Combine DataFrames
+        cluster_df = pd.concat(dfs)
 
         # Append to the list of all contrasts
         cluster_dfs.append(cluster_df)
 
     # Combine DataFrames of all contrasts
-    # TODO: Maybe we should convert this to long format for easier plotting
     cluster_df = pd.concat(cluster_dfs, ignore_index=True)
 
     return cluster_df
