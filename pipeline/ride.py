ac8de717416d1e5eb7fa629da147ff864cf68128
Kirsten Stark <kirsten.stark@posteo.de>
2023-11-03 13:13:00 +0100

    write cfg configeration in each iteration of loop

--- pipeline/ride.py
+++ pipeline/ride.py
@@ -12,26 +12,25 @@ def correct_ride(epochs, bad_ixs, ride_condition_column, ride_rt_column,
 
     good_ixs = [ix for ix in range(len(epochs)) if ix not in bad_ixs]
     epochs_good = epochs[good_ixs].copy()
-    metadata = epochs_good.metadata
 
     # TODO: Move all of this INSIDE the loop because the `r_latency` vector
     # is different for each condition
-
-    comp_name = ('s', 'r')
-    comp_twd = (ride_s_twd * 1000, ride_r_twd * 1000)  # Convert s to ms
-    comp_latency = (0.0, metadata[ride_rt_column].values)
+    comp_name = ['s', 'r']
+    comp_twd = [[x * 1000 for x in ride_s_twd], 
+                [x * 1000 for x in ride_r_twd]]  # Convert s to ms
     sfreq = epochs_good.info['sfreq']
-    epoch_twd = (epochs_good.tmin * 1000, epochs_good.tmax * 1000)
+    epoch_twd = [epochs_good.tmin * 1000, epochs_good.tmax * 1000]
     re_samp = 1000 / sfreq
     bl = np.abs(epochs_good.baseline[0]) * 1000
-
-    cfg = RideCfg(comp_name, comp_twd, comp_latency, sfreq, epoch_twd,
-                  re_samp=re_samp, bl=bl)
-
-    conditions = metadata[ride_condition_column].unique()
+    
+    conditions = epochs_good.metadata[ride_condition_column].unique()
     for condition in conditions:
-
-        condition_ixs = metadata[ride_condition_column] == condition
+        condition_ixs = epochs_good.metadata[ride_condition_column] == condition
         epochs_condition = epochs_good[condition_ixs].copy()
+        comp_latency = [0.0, epochs_condition.metadata[ride_rt_column].values]
 
+        cfg = RideCfg(comp_name, comp_twd, comp_latency, sfreq, epoch_twd,
+                    re_samp=re_samp, bl=bl)
+
+       
         ride_results = ride_call(epochs_condition, cfg)
